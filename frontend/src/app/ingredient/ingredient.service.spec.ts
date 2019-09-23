dc6386c20bbffc6e6e6486f06d6fedeca9c75f6b
Paul Dimalante <paul.dimalante@cognizant.com>
2019-09-23 10:45:30 -0400

    added service test (frontend)

---
+++ frontend/src/app/ingredient/ingredient.service.spec.ts
@@ -0,0 +1,77 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { IngredientService } from './ingredient.service';
+import { Ingredient } from './ingredient';
+import {
+  HttpTestingController,
+  HttpClientTestingModule
+} from "@angular/common/http/testing";
+import { environment } from "../../environments/environment";
+
+describe('IngredientService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [IngredientService]
+    });
+  });
+
+  it('should be created', inject([IngredientService], (service: IngredientService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('should return all ingredients by calling http service', 
+    inject([IngredientService, HttpTestingController], (service: IngredientService, httpMock: HttpTestingController) => {
+
+      const mockResponse = [
+        new Ingredient(1, "hot sauce"),
+        new Ingredient(2, "soy sauce")
+        
+      ]
+      
+      service.findAll().subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+      
+      const mockReq = httpMock.expectOne(environment.ingredientsURL + '/findAll');
+
+      mockReq.flush(mockResponse);
+
+      httpMock.verify();
+  }));
+
+  it('should return single ingredient by calling http service', 
+    inject([IngredientService, HttpTestingController], (service: IngredientService, httpMock: HttpTestingController) => {
+
+      const mockResponse = new Ingredient(1, "hot sauce");
+      
+      service.findById(1).subscribe((response) => {
+        expect(response).toEqual(mockResponse);
+      });
+      
+      const mockReq = httpMock.expectOne(environment.ingredientsURL + '/findById/1');
+
+      expect(mockReq.request.method).toEqual("GET");
+
+      mockReq.flush(mockResponse);
+
+      httpMock.verify();
+  }));
+
+  it('should post supplied ingredient via http', 
+  inject([IngredientService, HttpTestingController], (service: IngredientService, httpMock: HttpTestingController) => {
+    const mockArg = new Ingredient(1, "hot sauce");
+      
+    service.save(mockArg).subscribe();
+    
+    const mockReq = httpMock.expectOne(environment.ingredientsURL + '/save');
+
+    expect(mockReq.request.method).toEqual("POST");
+    expect(mockReq.request.body).toEqual(mockArg);
+
+    mockReq.flush({});
+
+    httpMock.verify();
+  }));
+
+});
---
+++ frontend/src/app/ingredient/ingredient.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Ingredient } from './ingredient';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IngredientService {
+  
+  constructor(private httpClient: HttpClient) { }
+
+  findById(id: number): Observable<Ingredient> {
+    return this.httpClient.get<Ingredient>(environment.ingredientsURL + '/findById/' + id);
+  }
+
+  findAll(): Observable<Array<Ingredient>> {
+    return this.httpClient.get<Array<Ingredient>>(environment.ingredientsURL + '/findAll');
+  }
+
+  save(ingredient: Ingredient): Observable<any> {
+    return this.httpClient.post(environment.ingredientsURL + '/save', ingredient);    
+  }
+
+}
